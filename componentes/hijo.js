d76361ec175bc3bfeacce015750ea8ca2efcbc1c
jose roman <el-1ero@outlook.com>
2020-02-07 16:17:39 -0400

    mapState, mapMutation y vuex

--- componentes/hijo.js
+++ componentes/hijo.js
@@ -2,9 +2,15 @@ Vue.component('hijo', {
     template: //html 
         `
     <div class="py-5">
-        <button @click="$store.commit('aumentar')">+</button>
-        <button @click="$store.commit('disminuir')">-</button>
-        <h1>numero {{$store.state.numero}}</h1>
+        <button @click="aumentar(2)">+</button>
+        <button @click="disminuir(2)">-</button>
+        <h1>numero {{numero}}</h1>
     </div>
-    `
+    `,
+    computed: {
+        ...Vuex.mapState(['numero'])
+    },
+    methods: {
+        ...Vuex.mapMutations(['aumentar', 'disminuir'])
+    }
 });
--- componentes/padre.js
+++ componentes/padre.js
@@ -2,8 +2,11 @@ Vue.component('padre', {
     template: //html 
         `
         <div>
-        <h1>numero {{$store.state.numero}}</h1>
+        <h1>numero {{numero}}</h1>
         <hijo></hijo>
     </div>
-    `
+    `,
+    computed: {
+        ...Vuex.mapState(['numero'])
+    }
 });
